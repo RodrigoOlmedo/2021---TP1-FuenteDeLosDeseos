7e7a4fa648802f74b8a1756269c3b74234601766
Rodrigo <rodrigomolmedo1@gmail.com>
2021-05-10 20:49:31 -0300

    Todo hecho

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,176 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+--Definimos equivalencias de tipos con el fin de tener un nombres mas representativos
+
+type SuenioCumplido = Persona -> Persona
+type PersonaModificada = Persona -> Persona
+type Ciudad = String 
+
+--modelamos el tipo de dato "Suenio" y permitimos poder mostrarlo en consola y compararlo
+
+data Suenio = Recibirse | Viajar | Enamorarse | QueTodoSigaIgual | QuierePackMedicina deriving (Show,Eq)
+
+--Definimos el tipo "Persona" utilizando Record Syntax, tipando las caracteristicas de la misma y permitiendo mostrar y comparar a la persona
+
+data Persona = UnaPersona{
+    edad :: Number
+,   suenios :: [Suenio]
+,   nombre :: String
+,   felicidonios :: Number
+,   habilidades :: [String]
+}deriving (Show,Eq)
+
+
+-- 1 a 
+
+coeficienteDeSatisfaccion :: Persona -> Number
+coeficienteDeSatisfaccion persona 
+    | felicidonios persona > 100 = felicidonios persona * edad persona
+    | felicidonios persona <= 50 = div (felicidonios persona) 2 
+    | otherwise = cantidadDeSuenios (suenios persona) * felicidonios persona
+
+
+{- A ESTO NO LE DES BOLA QUE NO ESTA EN EL CODIGO, LO QUISE HACER ASI PERO NO ENTENDI COMO RESOLVERLO
+
+
+data Felicidad = MuyFeliz  ModeradamenteFeliz  PocoFeliz deriving (Eq)
+
+coeficienteDeSatisfaccion  Persona - Number
+coeficienteDeSatisfaccion persona 
+     felicidad persona == MuyFeliz = felicidonios persona  edad persona
+     felicidad persona == PocoFeliz = div (felicidonios persona) 2 
+     otherwise = cantidadDeSuenios (suenios persona)  felicidonios persona
+
+esMuyFeliz  Persona - Felicidad
+esMuyFeliz persona = felicidonios persona 100 
+
+felicidad  Persona - Felicidad
+felicidad persona  felicidonios persona 100 = MuyFeliz
+                   felicidonios persona =50 = PocoFeliz
+                   otherwise = ModeradamenteFeliz 
+-}    
+
+--1b 
+gradoDeAmbicion :: Persona ->  Number
+gradoDeAmbicion persona | felicidonios persona > 100 = felicidonios persona * cantidadDeSuenios (suenios persona)
+                        | felicidonios persona <= 100 && felicidonios persona > 50 = (edad persona)* cantidadDeSuenios (suenios persona)
+                        | otherwise = cantidadDeSuenios (suenios persona) * 2
+
+cantidadDeSuenios :: [Suenio]->Number
+cantidadDeSuenios = length
+
+{- Respuestas c
+
+        - Son los tres estados de felicidad en la cual se puede encontrar una persona, dependiendo su cantidad de felicidonios
+
+        - Los casos de prueba estan hechos en base a las tres situaciones que se pueden dar dentro de las funciones
+        coeficienteDeSatisfaccion y gradoDeAmbicion, al tomar un valor dentro de cada rango podemos comprobar que, sin importar el caso,
+        toda la funcion funciona (valga la redundancia) correctamente 
+          
+-}
+
+-- 2 a 
+nombreLargo :: Persona -> Bool
+nombreLargo persona = length (nombre persona) > 10
+
+
+-- 2 b 
+boolPersonaSuertuda :: Persona -> Bool
+boolPersonaSuertuda =  even.(3*).coeficienteDeSatisfaccion --aplicacion parcial y composicion
+ 
+
+personaSuertuda :: Persona -> String
+personaSuertuda persona | boolPersonaSuertuda persona == True = "Es suertuda"
+                        | boolPersonaSuertuda persona == False = "No es suertuda"
+
+
+-- (Error 404: Tercer Integrante not found)
+
+terminaEnA :: String->Bool
+terminaEnA = ('a'==).last -- aplicacion parcial y composicion (Debate acerca de que tan util es si cambia el parametro)
+
+nombreLindo :: Persona -> Bool
+nombreLindo persona = terminaEnA (nombre persona)
+
+--3 PARTE A
+
+seRecibio :: String -> PersonaModificada -- se recibe pero no necesariamente es su sueño 
+seRecibio carrera persona = cumplioRecibirse carrera (habilidadExistente carrera persona) (sueniaRecibirse persona) persona
+
+sueniaRecibirse :: Persona -> Bool --Evaluamos si su suenio es recibirse
+sueniaRecibirse persona =  elem Recibirse (suenios persona)||elem QuierePackMedicina (suenios persona)
+
+cumplioRecibirse :: String -> Bool -> Bool -> SuenioCumplido --Si cada sueño fuese validado, definiriamos: SuenioCumplido = Bool->Persona->Persona
+cumplioRecibirse carrera condicion False persona = persona  --Utilizamos Pattern Matching para evaluar condiciones
+cumplioRecibirse carrera False True persona  = UnaPersona {
+        edad = edad persona
+    ,   suenios = suenios persona
+    ,   nombre = nombre persona
+    ,   felicidonios = felicidonios persona + 1000 * length carrera
+    ,   habilidades = carrera:habilidades persona }
+
+cumplioRecibirse carrera True True persona  = UnaPersona {
+        edad = edad persona
+    ,   suenios = suenios persona
+    ,   nombre = nombre persona
+    ,   felicidonios = felicidonios persona + 1000 * length carrera
+    ,   habilidades = habilidades persona }
+
+habilidadExistente :: String -> Persona -> Bool --comprobamos si tiene como habilidad la carrera
+habilidadExistente carrera persona = elem carrera (habilidades persona)
+
+
+viajo :: [Ciudad]->SuenioCumplido
+viajo ciudades persona= UnaPersona{
+    edad = edad persona +1
+,   suenios = suenios persona
+,   nombre = nombre persona
+,   felicidonios = felicidonios persona + 100 * cantidadDeCiudades ciudades
+,   habilidades = habilidades persona
+    
+} 
+
+cantidadDeCiudades :: [Ciudad]->Number 
+cantidadDeCiudades = length
+
+--(Error 404: Tercer Integrante not Found)
+
+seEnamoro :: Persona -> SuenioCumplido
+seEnamoro personaY personaX = UnaPersona{
+    edad = edad personaX
+,   suenios = suenios personaX
+,   nombre = nombre personaX
+,   felicidonios = felicidonios personaX + felicidonios personaY --sumamos felicidonios de la otra persona
+,   habilidades = habilidades personaX
+}
+
+-- 3 PARTE B
+
+todoSigueIgual :: SuenioCumplido
+todoSigueIgual persona = persona --no cambia
+
+packMedicina :: SuenioCumplido
+packMedicina = bonus.viajeMedicinal.esMedico --composicion
+
+esMedico :: PersonaModificada --se recibe de medicina
+esMedico = seRecibio "Medicina"
+
+viajeMedicinal :: PersonaModificada --viaja a 2 ciudades
+viajeMedicinal = viajo ["Berazategui","Paris"]
+
+bonus :: PersonaModificada --Suma 100 felicidonios
+bonus persona = UnaPersona{
+    edad = edad persona
+,   suenios = suenios persona
+,   nombre = nombre persona
+,   felicidonios = felicidonios persona + 100
+,   habilidades = habilidades persona
+}
+
+triple :: Suenio-> [Ciudad] -> String -> Persona -> SuenioCumplido 
+triple QuierePackMedicina _ _ _= packMedicina.packMedicina.packMedicina --Utilizamos Pattern Matching para definir el sueño a triplicar
+triple QueTodoSigaIgual _ _ _= todoSigueIgual.todoSigueIgual.todoSigueIgual
+triple Enamorarse _ _ personaB = (seEnamoro personaB).(seEnamoro personaB).(seEnamoro personaB)
+triple Recibirse _ carrera _ = (seRecibio carrera).(seRecibio carrera).(seRecibio carrera)
+triple Viajar suenios _ _ = (viajo suenios).(viajo suenios).(viajo suenios)
--- src/Spec.hs
+++ src/Spec.hs
@@ -3,9 +3,81 @@ import PdePreludat
 import Library
 import Test.Hspec
 
+--Definimos una persona generica para simplificar los casos de prueba
+pepe = UnaPersona{
+    edad = 23
+,   suenios = [Recibirse]
+,   nombre = "Fransisca"
+,   felicidonios = 45
+,   habilidades = []
+}
+
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+  describe "Coeficiente de Satisfaccion" $ do
+    it "El coeficiente de satisfacción de una persona muy feliz con 101 felicidonios y 25 años debe ser 2.525" $ do
+      coeficienteDeSatisfaccion (UnaPersona 25 [] "Maria" 101 []) `shouldBe` 2525
+    it "El coeficiente de satisfacción de una persona moderadamente feliz con 100 felicidonios y 2 sueños debe ser 200" $ do
+      coeficienteDeSatisfaccion (UnaPersona 24 [Recibirse, Viajar] "Juan" 100 []) `shouldBe` 200
+    it "El coeficiente de satisfacción de una persona poco feliz con 50 felicidonios debe ser 25" $ do
+      coeficienteDeSatisfaccion (UnaPersona 50 [] "Pedro" 50 []) `shouldBe` 25
 
+  describe "Grado de Ambicion" $ do
+    it "El grado de ambición de una persona muy feliz con 101 felicidonios y 2 sueños debe ser 202" $ do
+      gradoDeAmbicion (UnaPersona 26 [QuierePackMedicina, QueTodoSigaIgual] "Alf" 101 []) `shouldBe` 202
+    it "El grado de ambición de una persona moderadamente feliz con 100 felicidonios, 26 años y 2 sueños debe ser 52" $ do
+      gradoDeAmbicion (UnaPersona 26 [QuierePackMedicina, Viajar] "Alf" 100 []) `shouldBe` 52
+    it "El grado de ambición de una persona poco feliz con 50 felicidonios y 1 sueño debe ser 2" $ do
+      gradoDeAmbicion (UnaPersona 26 [Recibirse] "Alf" 50 []) `shouldBe` 2
+
+  describe "Es un Nombre Largo" $ do
+    it "El nombre Evangelina no es un nombre largo" $ do
+      nombreLargo (UnaPersona 26 [QuierePackMedicina, QueTodoSigaIgual] "Evangelina" 101 []) `shouldBe` False
+    it "El nombre Maximiliano es un nombre largo" $ do
+      nombreLargo (UnaPersona 26 [QuierePackMedicina, QueTodoSigaIgual] "Maixmiliano" 101 []) `shouldBe` True
+
+  describe "Persona Suertuda" $ do
+    it "Una persona con 14 felicidonios no es suertuda" $ do
+      personaSuertuda (UnaPersona 26 [QuierePackMedicina, QueTodoSigaIgual] "Maixmiliano" 14 []) `shouldBe` "No es suertuda"
+    it "Una persona con 12 felicidonios es suertuda" $ do
+      personaSuertuda (UnaPersona 26 [QuierePackMedicina, QueTodoSigaIgual] "Maixmiliano" 12 []) `shouldBe` "Es suertuda"
+
+  describe "Es un Nombre Lindo" $ do
+    it "Una persona Ariel no tiene nombre lindo" $ do
+      nombreLindo (UnaPersona 30 [QuierePackMedicina, QueTodoSigaIgual] "Ariel" 14 []) `shouldBe` False
+    it "Una persona Melina tiene nombre lindo" $ do
+      nombreLindo (UnaPersona 21 [QuierePackMedicina, QueTodoSigaIgual] "Melina" 12 []) `shouldBe` True
+
+  describe "Se Recibio" $ do
+    it "Una persona que sueña recibirse y se recibe de ingenieria debe recibir 10000 feliciondios y conseguir la habilidad" $ do
+      seRecibio "Ingenieria" (UnaPersona 21 [Recibirse] "Juan" 100 []) `shouldBe` (UnaPersona 21 [Recibirse] "Juan" 10100 ["Ingenieria"])
+    it "Una persona que no sueña con recibirse y se recibe no debe cambiar" $ do
+      seRecibio "Carrera" (UnaPersona 21 [] "Juan" 100 []) `shouldBe` (UnaPersona 21 [] "Juan" 100 [])
+  
+  describe "Logro Viajar" $ do
+    it "Una persona que viaja a dos ciudades debe sumar 200 felicidonios y sumar un año" $ do
+      viajo ["madrid", "barcelona"] pepe `shouldBe` (UnaPersona 24 [Recibirse] "Fransisca" 245 [])
+  
+  describe "Se enamoro" $ do
+    it "Una persona que se enamoro de una persona con 100 felicidonios debe sumar esa misma cantidad" $ do
+      seEnamoro (UnaPersona 21 [] "Micaela" 100 []) pepe `shouldBe` (UnaPersona 23 [Recibirse] "Fransisca" 145 [])
+
+  describe "Todo Sigue Igual" $ do
+    it "Una persona que quiere que todo siga igual no debe cambiar" $ do
+      todoSigueIgual pepe `shouldBe` pepe
+
+  describe "Pack Medicina" $ do
+    it "Una persona que consigue el pack medicinal debe sumar 8300 felicidonios, sumar un año y conseguir la habilidad de Medicina" $ do
+      packMedicina pepe `shouldBe` (UnaPersona 24 [Recibirse] "Fransisca" 8345 ["Medicina"])
+
+  describe "Triplicar Efectos" $ do
+    it "Una persona que quiere triplicar el sueño del pack medicinal debe sumar 24900 felicidonios, sumar tres años y conseguir la habilidad de Medicina" $ do
+      triple QuierePackMedicina [] "" pepe pepe `shouldBe` (UnaPersona 26 [Recibirse] "Fransisca" 24945 ["Medicina"])
+    it "Una persona que quiere triplicar el sueño de viajar a dos ciudades debe sumar 600 felicidonios y sumar tres años" $ do
+      triple Viajar ["madrid", "Quilmes"] "" pepe pepe `shouldBe` (UnaPersona 26 [Recibirse] "Fransisca" 645 [])
+    it "Una persona que quiere triplicar el sueño de recibirse de abogado debe sumar 21000 felicidonios y conseguir la habilidad de abogado" $ do
+      triple Recibirse [] "abogado" pepe pepe `shouldBe` (UnaPersona 23 [Recibirse] "Fransisca" 21045 ["abogado"])
+    it "Una persona que quiere triplicar el sueño de enamorarse de una persona con 45 felicidonios debe sumar 135 felicidonios" $ do
+      triple Enamorarse [] " " pepe pepe `shouldBe` (UnaPersona 23 [Recibirse] "Fransisca" 180 [])
+    it "Una persona que quiere triplicar el sueño de que todo siga igual no debe cambiar" $ do
+      triple QueTodoSigaIgual [] " " pepe pepe `shouldBe` pepe
