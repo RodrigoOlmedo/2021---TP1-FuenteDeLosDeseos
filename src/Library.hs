0512b662a056f934a5e5349db793012cacab640d
Rodrigo <rodrigomolmedo1@gmail.com>
2021-05-27 20:23:40 -0300

    Agrega lo que quieras

--- src/Library.hs
+++ src/Library.hs
@@ -3,13 +3,13 @@ import PdePreludat
 
 --Definimos equivalencias de tipos con el fin de tener un nombres mas representativos
 
-type SuenioCumplido = Persona -> Persona
-type PersonaModificada = Persona -> Persona
+type Suenio = Persona -> Persona
+type Fuente = Persona -> Persona
 type Ciudad = String 
 
 --modelamos el tipo de dato "Suenio" y permitimos poder mostrarlo en consola y compararlo
 
-data Suenio = Recibirse | Viajar | Enamorarse | QueTodoSigaIgual | QuierePackMedicina deriving (Show,Eq)
+--data Suenio = Recibirse | Viajar | Enamorarse | QueTodoSigaIgual | QuierePackMedicina deriving (Show,Eq)
 
 --Definimos el tipo "Persona" utilizando Record Syntax, tipando las caracteristicas de la misma y permitiendo mostrar y comparar a la persona
 
@@ -21,68 +21,61 @@ data Persona = UnaPersona{
 ,   habilidades :: [String]
 }deriving (Show,Eq)
 
-
+pepe = UnaPersona{
+    edad = 23
+,   suenios = [recibirse "medicina", viajar ["tailandia"]]
+,   nombre = "Fransisca"
+,   felicidonios = 45
+,   habilidades = []
+}
 -- 1 a 
 
+esMuyFeliz :: Persona -> Bool
+esMuyFeliz = (>100).felicidonios
+
+esModeradamenteFeliz :: Persona -> Bool
+esModeradamenteFeliz persona = felicidonios persona >50 && felicidonios persona <=100
+--esModeradamenteFeliz = (>50).felicidonios se podria hacer esto porque no entraria a la condicion en la guarda, pero no conviene como concepto
+
+esPocoFeliz :: Persona -> Bool
+esPocoFeliz = (<50).felicidonios
+
+
 coeficienteDeSatisfaccion :: Persona -> Number
-coeficienteDeSatisfaccion persona 
-    | felicidonios persona > 100 = felicidonios persona * edad persona
-    | felicidonios persona <= 50 = div (felicidonios persona) 2 
-    | otherwise = cantidadDeSuenios (suenios persona) * felicidonios persona
-
-
-{- A ESTO NO LE DES BOLA QUE NO ESTA EN EL CODIGO, LO QUISE HACER ASI PERO NO ENTENDI COMO RESOLVERLO
-
-
-data Felicidad = MuyFeliz  ModeradamenteFeliz  PocoFeliz deriving (Eq)
-
-coeficienteDeSatisfaccion  Persona - Number
-coeficienteDeSatisfaccion persona 
-     felicidad persona == MuyFeliz = felicidonios persona  edad persona
-     felicidad persona == PocoFeliz = div (felicidonios persona) 2 
-     otherwise = cantidadDeSuenios (suenios persona)  felicidonios persona
-
-esMuyFeliz  Persona - Felicidad
-esMuyFeliz persona = felicidonios persona 100 
-
-felicidad  Persona - Felicidad
-felicidad persona  felicidonios persona 100 = MuyFeliz
-                   felicidonios persona =50 = PocoFeliz
-                   otherwise = ModeradamenteFeliz 
--}    
+coeficienteDeSatisfaccion persona  | esMuyFeliz persona = felicidonios persona * edad persona
+                                   | esModeradamenteFeliz persona =cantidadDeSuenios persona * felicidonios persona 
+                                   | otherwise = div (felicidonios persona) 2 
 
 --1b 
 gradoDeAmbicion :: Persona ->  Number
-gradoDeAmbicion persona | felicidonios persona > 100 = felicidonios persona * cantidadDeSuenios (suenios persona)
-                        | felicidonios persona <= 100 && felicidonios persona > 50 = (edad persona)* cantidadDeSuenios (suenios persona)
-                        | otherwise = cantidadDeSuenios (suenios persona) * 2
+gradoDeAmbicion persona | esMuyFeliz persona = felicidonios persona * cantidadDeSuenios persona
+                        | esModeradamenteFeliz persona = (edad persona)* cantidadDeSuenios persona
+                        | otherwise = cantidadDeSuenios persona * 2
 
-cantidadDeSuenios :: [Suenio]->Number
-cantidadDeSuenios = length
+cantidadDeSuenios :: Persona->Number
+cantidadDeSuenios = length.suenios
 
 {- Respuestas c
 
-        - Son los tres estados de felicidad en la cual se puede encontrar una persona, dependiendo su cantidad de felicidonios
+        - Son los tres estados de felicidad en la cual se puede encontrar una persona, dependiendo su cantidad de felicidonios. Son las 
+        Clases de Equivalencia que puede adoptar la funcion.
 
         - Los casos de prueba estan hechos en base a las tres situaciones que se pueden dar dentro de las funciones
         coeficienteDeSatisfaccion y gradoDeAmbicion, al tomar un valor dentro de cada rango podemos comprobar que, sin importar el caso,
-        toda la funcion funciona (valga la redundancia) correctamente 
+        toda la funcion funciona (valga la redundancia) correctamente. Ademas, tomamos los valores límites
+        donde la función cambia su comportamiento y es mas probable que se rompa.
           
 -}
 
 -- 2 a 
 nombreLargo :: Persona -> Bool
-nombreLargo persona = length (nombre persona) > 10
+nombreLargo = (>10).length.nombre
 
 
 -- 2 b 
-boolPersonaSuertuda :: Persona -> Bool
-boolPersonaSuertuda =  even.(3*).coeficienteDeSatisfaccion --aplicacion parcial y composicion
+esPersonaSuertuda :: Persona -> Bool
+esPersonaSuertuda =  even.(3*).coeficienteDeSatisfaccion --aplicacion parcial y composicion
  
-
-personaSuertuda :: Persona -> String
-personaSuertuda persona | boolPersonaSuertuda persona == True = "Es suertuda"
-                        | boolPersonaSuertuda persona == False = "No es suertuda"
 
 
 -- (Error 404: Tercer Integrante not found)
@@ -91,86 +84,65 @@ terminaEnA :: String->Bool
 terminaEnA = ('a'==).last -- aplicacion parcial y composicion (Debate acerca de que tan util es si cambia el parametro)
 
 nombreLindo :: Persona -> Bool
-nombreLindo persona = terminaEnA (nombre persona)
+nombreLindo = terminaEnA.nombre
 
 --3 PARTE A
+agregarFelicidonios :: Number->Persona -> Persona
+agregarFelicidonios felicidoniosASumar persona= persona {felicidonios = felicidonios persona + felicidoniosASumar}
 
-seRecibio :: String -> PersonaModificada -- se recibe pero no necesariamente es su sueño 
-seRecibio carrera persona = cumplioRecibirse carrera (habilidadExistente carrera persona) (sueniaRecibirse persona) persona
+agregarHabilidad :: String -> Persona -> Persona
+agregarHabilidad habilidad persona= persona {habilidades = habilidad :habilidades persona}
 
-sueniaRecibirse :: Persona -> Bool --Evaluamos si su suenio es recibirse
-sueniaRecibirse persona =  elem Recibirse (suenios persona)||elem QuierePackMedicina (suenios persona)
+sumarAnios :: Number -> Persona -> Persona
+sumarAnios anios persona = persona {edad=edad persona+anios}
 
-cumplioRecibirse :: String -> Bool -> Bool -> SuenioCumplido --Si cada sueño fuese validado, definiriamos: SuenioCumplido = Bool->Persona->Persona
-cumplioRecibirse carrera condicion False persona = persona  --Utilizamos Pattern Matching para evaluar condiciones
-cumplioRecibirse carrera False True persona  = UnaPersona {
-        edad = edad persona
-    ,   suenios = suenios persona
-    ,   nombre = nombre persona
-    ,   felicidonios = felicidonios persona + 1000 * length carrera
-    ,   habilidades = carrera:habilidades persona }
+recibirse :: String -> Suenio 
+recibirse carrera = (agregarHabilidad carrera).(agregarFelicidonios (1000*length carrera))
 
-cumplioRecibirse carrera True True persona  = UnaPersona {
-        edad = edad persona
-    ,   suenios = suenios persona
-    ,   nombre = nombre persona
-    ,   felicidonios = felicidonios persona + 1000 * length carrera
-    ,   habilidades = habilidades persona }
-
-habilidadExistente :: String -> Persona -> Bool --comprobamos si tiene como habilidad la carrera
-habilidadExistente carrera persona = elem carrera (habilidades persona)
-
-
-viajo :: [Ciudad]->SuenioCumplido
-viajo ciudades persona= UnaPersona{
-    edad = edad persona +1
-,   suenios = suenios persona
-,   nombre = nombre persona
-,   felicidonios = felicidonios persona + 100 * cantidadDeCiudades ciudades
-,   habilidades = habilidades persona
-    
-} 
+viajar :: [Ciudad]->Suenio
+viajar ciudades = (sumarAnios 1).(agregarFelicidonios (100*cantidadDeCiudades ciudades)) 
 
 cantidadDeCiudades :: [Ciudad]->Number 
 cantidadDeCiudades = length
 
 --(Error 404: Tercer Integrante not Found)
 
-seEnamoro :: Persona -> SuenioCumplido
-seEnamoro personaY personaX = UnaPersona{
-    edad = edad personaX
-,   suenios = suenios personaX
-,   nombre = nombre personaX
-,   felicidonios = felicidonios personaX + felicidonios personaY --sumamos felicidonios de la otra persona
-,   habilidades = habilidades personaX
-}
+enamorarse :: Persona -> Suenio
+enamorarse personaY  = (agregarFelicidonios (felicidonios personaY))
 
 -- 3 PARTE B
 
-todoSigueIgual :: SuenioCumplido
-todoSigueIgual persona = persona --no cambia
+queTodoSiguaIgual :: Suenio
+queTodoSiguaIgual = id--no cambia
 
-packMedicina :: SuenioCumplido
+packMedicina :: Suenio
 packMedicina = bonus.viajeMedicinal.esMedico --composicion
 
-esMedico :: PersonaModificada --se recibe de medicina
-esMedico = seRecibio "Medicina"
+esMedico :: Suenio --se recibe de medicina
+esMedico = recibirse "Medicina"
 
-viajeMedicinal :: PersonaModificada --viaja a 2 ciudades
-viajeMedicinal = viajo ["Berazategui","Paris"]
+viajeMedicinal :: Suenio --viaja a 2 ciudades
+viajeMedicinal = viajar ["Berazategui","Paris"]
 
-bonus :: PersonaModificada --Suma 100 felicidonios
-bonus persona = UnaPersona{
-    edad = edad persona
-,   suenios = suenios persona
-,   nombre = nombre persona
-,   felicidonios = felicidonios persona + 100
-,   habilidades = habilidades persona
-}
+bonus :: Persona->Persona --Suma 100 felicidonios
+bonus = agregarFelicidonios 100
 
-triple :: Suenio-> [Ciudad] -> String -> Persona -> SuenioCumplido 
-triple QuierePackMedicina _ _ _= packMedicina.packMedicina.packMedicina --Utilizamos Pattern Matching para definir el sueño a triplicar
-triple QueTodoSigaIgual _ _ _= todoSigueIgual.todoSigueIgual.todoSigueIgual
-triple Enamorarse _ _ personaB = (seEnamoro personaB).(seEnamoro personaB).(seEnamoro personaB)
-triple Recibirse _ carrera _ = (seRecibio carrera).(seRecibio carrera).(seRecibio carrera)
-triple Viajar suenios _ _ = (viajo suenios).(viajo suenios).(viajo suenios)
+triple :: Suenio -> Suenio 
+triple suenio = suenio.suenio.suenio
+
+fuenteMinimalista :: Fuente
+fuenteMinimalista persona = sacarPrimerSuenio ((enesimoSuenio 0 persona) persona)
+
+--primerSuenio persona = head suenios persona
+--cumplirSuenio suenio = suenio QUIERO USAR COMPOSICION EN FUENTEMINIMALISTA Y NO SE COMO :(
+
+enesimoSuenio :: Number->Persona -> Suenio
+enesimoSuenio numero persona = (!!) (suenios persona) numero
+
+sacarPrimerSuenio :: Persona->Persona
+sacarPrimerSuenio persona = persona{suenios = tail (suenios persona)}
+
+fuenteSorda :: Fuente
+fuenteSorda = queTodoSiguaIgual
+
+fuenteGanadoraSegun :: (Persona->Number)->
